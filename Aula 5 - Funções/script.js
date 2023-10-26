f0cdadfaa254d8edb8b2658809061fda99af3373
Rafael Novais <rbnovais@outlook.com>
2023-10-26 16:05:17 -0300

    Calculadora-em-JS

---
+++ Aula 5 - Funções/script.js
@@ -0,0 +1,10 @@
+/*function funcao(){
+    console.log('estamos no caminho!')
+}
+funcao();*/
+
+//parâmetros
+function mensagem(primeiro, segundo){
+    console.log(primeiro, segundo)
+}
+mensagem('Tudo certo,', 'jovem!')
---
+++ Aula 6 - Aprofundando em funções/tiposdefuncao.js
@@ -0,0 +1,26 @@
+//TIPOS DE FUNÇÃO
+
+//1 - declarativa
+/*function função(){
+    console.log('Esta é uma função declarativa')
+}
+função ();*/
+
+//2 - Expressões de funções
+    //com nomeação
+/*var função = function função(){
+    console.log('Esta é uma expressão de função!')
+}
+função()*/
+
+    //sem nomeação
+/*var função = function(){
+    console.log('Esta é uma expressão de função!')
+}
+função()*/
+
+//3 - Arrow function
+/*var funcao = () => {
+    console.log('Esta é uma arrow function')
+}
+funcao();*/
